79c68c301a3a1c129f2a359be81e790b791a38ad
ay05 <Aiden.Q.Yu@newegg.com>
2020-08-04 14:47:36 +0800

    git init

---
+++ src/main/java/com/newegg/controller/EggMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.newegg.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggMeetingApplication.class, args);
+	}
+
+}
